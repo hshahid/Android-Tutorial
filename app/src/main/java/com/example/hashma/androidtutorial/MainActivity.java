2aced5c89387df5145aff05eb46c65bc6f2fba6d
Hashma <shahi22h@mtholyoke.edu>
2016-06-21 10:12:55 +0500

    “update”

--- app/src/main/java/com/example/hashma/androidtutorial/MainActivity.java
+++ app/src/main/java/com/example/hashma/androidtutorial/MainActivity.java
@@ -5,10 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -53,8 +52,8 @@ public class MainActivity extends AppCompatActivity {
 
     // fired when the button is clicked
     public void onSubmit(View view) {
-        // show a "Hello World" message on screen
-        // a Toast == "simple alert"
-        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
+        // get value from textField
+        // set value into the label
+        //display value as a quick alert
     }
 }
